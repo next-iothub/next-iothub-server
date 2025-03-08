65abd609050c1b2d34ad1396ecc24c13d45489fc
Austin <wsl612425@163.com>
2025-03-08 19:08:34 +0800

    initial commit

---
+++ src/main/java/com/wangsl/IothubServerApplication.java
@@ -0,0 +1,13 @@
+package com.wangsl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IothubServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IothubServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wangsl/IothubServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wangsl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IothubServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
